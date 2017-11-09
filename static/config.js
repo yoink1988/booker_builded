7f4db4d38f8a584cb0fce6782b426115018aa7a7
Ilya Erinchak <yoink1988@gmail.com>
2017-11-10 01:16:20 +0200

    builded

---
+++ static/config.js
@@ -0,0 +1,9 @@
+var urlHome = 'http://localhost/booker/client/api/'
+var urlClass = 'http://192.168.0.15/~user9/booker/client/api/'
+
+function getUrl() {
+  return urlClass
+}
+
+const ADMIN = '2'
+const USER = '1'
